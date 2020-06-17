1e48000d3c7320511a31e99b198c33bdc8867d76
kzkunuzbam <muratkunuzbaev@gmail.com>
2020-06-17 16:04:40 +0600

    Added some new features. Deleted API key

--- youtube-onedaybuild/Helpers/Constants.swift
+++ youtube-onedaybuild/Helpers/Constants.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 struct Constants {
-    static var API_KEY = "AIzaSyCm3BUDKRSRMxoif_yvGdFaL3RMXY5ouy0"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "UUGg988WhG6MICDcgBbHM4zA"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
